9a3e70c71d3c3a77d4ec5b4171ab2ed62e2b254f
Daniel Cox <daniel.cox@deciphernow.com>
2018-04-08 17:46:36 -0400

    orienting cleanup

--- src/index.js
+++ src/index.js
@@ -1,134 +1,99 @@
 import { jsPlumb } from 'jsplumb'
 
-jsPlumb.bind("jsPlumbDemoLoaded", function (instance) {
 
-    var renderer = jsPlumbToolkit.Support.ingest({
-        jsPlumb: instance
+// Configuration
+//
+
+const colorRed = "rgb(255,59,48)";
+const colorBlue = "rgb(0,122,255)";
+const colorGreen = "rgb(76,217,100)";
+const defaultConnectionCurviness = 50;
+const InputColor = colorRed;
+const OutputEndpointColor = colorGreen;
+
+const instanceOptions = {
+  DragOptions: { cursor: 'pointer', zIndex: 2000 },
+  PaintStyle: { stroke: colorGreen },
+  EndpointHoverStyle: { fill: colorBlue },
+  HoverPaintStyle: { stroke: colorBlue },
+  EndpointStyle: { width: 16, height: 16, stroke: colorGreen },
+  Endpoint: "Dot",
+  Anchors: ["TopCenter", "TopCenter"],
+  Container: "canvas"
+};
+
+const exampleDropOptions = {
+  tolerance: "touch",
+  hoverClass: "dropHover",
+  activeClass: "dragActive"
+};
+
+const InputEndpoint = {
+  endpoint: ["Dot", { radius: 8 }],
+  paintStyle: { fill: colorGreen },
+  isSource: true,
+  isTarget: true,
+  scope: "probabilityConnection",
+  connectorStyle: { stroke: colorGreen, strokeWidth: 2 },
+  connector: ["Bezier", { curviness: defaultConnectionCurviness }],
+  maxConnections: -1,
+  dropOptions: exampleDropOptions
+};
+
+const OutputEndpoint = {
+  endpoint: ["Dot", { radius: 8 }],
+  paintStyle: { fill: colorGreen },
+  isSource: true,
+  isTarget: true,
+  scope: "probabilityConnection",
+  connectorStyle: { stroke: colorGreen, strokeWidth: 2 },
+  connector: ["Bezier", { curviness: defaultConnectionCurviness }],
+  maxConnections: -1,
+  dropOptions: exampleDropOptions
+};
+
+
+// Update Connections callback
+//
+
+const updateConnections = function (conn, isRemoval) {
+  console.log("[DEBUG] ", "New connection info: ", isRemoval?"(removal)":"", conn);
+};
+
+
+// Run jsPlumb
+//
+
+jsPlumb.ready(function () {
+
+  // instantiate jsPlumb instance
+  var instance = jsPlumb.getInstance(instanceOptions);
+
+  instance.batch(function () {
+
+    // event bindings
+    instance.bind("connection", function (info, originalEvent) {
+      updateConnections(info.connection);
+    });
+    instance.bind("connectionDetached", function (info, originalEvent) {
+      updateConnections(info.connection, true);
+    });
+    instance.bind("connectionMoved", function (info, originalEvent) {
+      updateConnections(info.connection, true);
     });
 
-    // bind to the node added event and tell the renderer to ingest each one
+    instance.bind("click", function (component, originalEvent) {
+      alert("click!")
+    });
 
-    instance.bind("jsPlumbDemoNodeAdded", function (el) { renderer.ingest(el); });
+    // make .window divs draggable
+    instance.draggable(jsPlumb.getSelector(".drag-drop-demo .window"));
+
+    // add input and output endpoints.
+    instance.addEndpoint(jsPlumb.getSelector(".drag-drop-demo .window"), { anchor: "LeftMiddle" }, InputEndpoint);
+    instance.addEndpoint(jsPlumb.getSelector(".drag-drop-demo .window"), { anchor: "RightMiddle" }, OutputEndpoint);
+  });
+
+
 });
-
-;
-(function () {
-
-    var listDiv = document.getElementById("list"),
-
-        showConnectionInfo = function (s) {
-            listDiv.innerHTML = s;
-            listDiv.style.display = "block";
-        },
-        hideConnectionInfo = function () {
-            listDiv.style.display = "none";
-        },
-        connections = [],
-        updateConnections = function (conn, remove) {
-            if (!remove) connections.push(conn);
-            else {
-                var idx = -1;
-                for (var i = 0; i < connections.length; i++) {
-                    if (connections[i] == conn) {
-                        idx = i;
-                        break;
-                    }
-                }
-                if (idx != -1) connections.splice(idx, 1);
-            }
-            if (connections.length > 0) {
-                var s = "<span><strong>Connections</strong></span><br/><br/><table><tr><th>Scope</th><th>Source</th><th>Target</th></tr>";
-                for (var j = 0; j < connections.length; j++) {
-                    s = s + "<tr><td>" + connections[j].scope + "</td>" + "<td>" + connections[j].sourceId + "</td><td>" + connections[j].targetId + "</td></tr>";
-                }
-                showConnectionInfo(s);
-            } else
-                hideConnectionInfo();
-        };
-
-    jsPlumb.ready(function () {
-
-        const colorRed = "rgb(255,59,48)";
-        const colorBlue = "rgb(0,122,255)";
-        const colorGreen = "rgb(76,217,100)";
-        const defaultConnectionCurviness = 50;
-        const InputColor = colorRed;
-        const OutputEndpointColor = colorGreen;
-
-        var instance = jsPlumb.getInstance({
-            DragOptions: { cursor: 'pointer', zIndex: 2000 },
-            PaintStyle: { stroke: colorGreen },
-            EndpointHoverStyle: { fill: colorBlue },
-            HoverPaintStyle: { stroke: colorBlue },
-            EndpointStyle: { width: 16, height: 16, stroke: colorGreen },
-            Endpoint: "Dot",
-            Anchors: ["TopCenter", "TopCenter"],
-            Container: "canvas"
-        });
-
-        // suspend drawing and initialise.
-        instance.batch(function () {
-
-            // bind to connection/connectionDetached events, and update the list of connections on screen.
-            instance.bind("connection", function (info, originalEvent) {
-                updateConnections(info.connection);
-            });
-            instance.bind("connectionDetached", function (info, originalEvent) {
-                updateConnections(info.connection, true);
-            });
-
-            instance.bind("connectionMoved", function (info, originalEvent) {
-                //  only remove here, because a 'connection' event is also fired.
-                // in a future release of jsplumb this extra connection event will not
-                // be fired.
-                updateConnections(info.connection, true);
-            });
-
-            instance.bind("click", function (component, originalEvent) {
-                alert("click!")
-            });
-
-            // configure some drop options for use by all endpoints.
-            var exampleDropOptions = {
-                tolerance: "touch",
-                hoverClass: "dropHover",
-                activeClass: "dragActive"
-            };
-
-            var InputEndpoint = {
-                endpoint: ["Dot", { radius: 8 }],
-                paintStyle: { fill: colorGreen },
-                isSource: true,
-                isTarget: true,
-                scope: "probabilityConnection",
-                connectorStyle: { stroke: colorGreen, strokeWidth: 2 },
-                connector: ["Bezier", { curviness: defaultConnectionCurviness }],
-                maxConnections: -1,
-                dropOptions: exampleDropOptions
-            };
-
-            var OutputEndpoint = {
-                endpoint: ["Dot", { radius: 8 }],
-                paintStyle: { fill: colorGreen },
-                isSource: true,
-                isTarget: true,
-                scope: "probabilityConnection",
-                connectorStyle: { stroke: colorGreen, strokeWidth: 2 },
-                connector: ["Bezier", { curviness: defaultConnectionCurviness }],
-                maxConnections: -1,
-                dropOptions: exampleDropOptions
-            };
-
-            // make .window divs draggable
-            instance.draggable(jsPlumb.getSelector(".drag-drop-demo .window"));
-
-            // add input and output endpoints.
-            instance.addEndpoint(jsPlumb.getSelector(".drag-drop-demo .window"), { anchor: "LeftMiddle" }, InputEndpoint);
-            instance.addEndpoint(jsPlumb.getSelector(".drag-drop-demo .window"), { anchor: "RightMiddle" }, OutputEndpoint);
-        });
-
-        jsPlumb.fire("jsPlumbDemoLoaded", instance);
-
-    });
-})();
 
